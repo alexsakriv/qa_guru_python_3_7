135a68ae037450b2b5c2c5bc7082d1f8dfa5a94e
krivorucenkoaleksandra <alexsakriv@gmail.com>
2023-03-01 20:55:51 +0300

    1) тест на с разметкой сценария allure.step

--- tests/test_issues_name.py
+++ tests/test_issues_name.py
@@ -1,6 +1,7 @@
 import allure
 from selene.support.shared import browser
 from selene import have
+from allure_commons.types import Severity
 
 
 def test_name_issue_with_selene():
@@ -14,7 +15,24 @@ def test_name_issue_with_selene():
 
     browser.element('.codesearch-results').should(have.text('#72'))
 
-def test_name_issue_with_lambda_steps():
+
+def test_name_issue_with_allure_step():
+    with allure.step('Открываем главную страницу'):
+        browser.open('https://github.com')
+
+    with allure.step('Ищем репозиторий'):
+        browser.element('.header-search-input').click()
+        browser.element('.header-search-input').type('eroshenkoam/allure-example')
+        browser.element('.header-search-input').submit()
+
+    with allure.step('Открываем Issues'):
+        browser.all('.menu-item').element_by(have.text('Issues')).click()
+
+    with allure.step('Проверяем наличие Issue с номером'):
+        browser.element('.codesearch-results').should(have.text('#72'))
+
+
+def test_name_issue_allure_step():
     open_main_page()
     search_for_repository('eroshenkoam/allure-example')
     open_issues_tab()
@@ -35,9 +53,17 @@ def search_for_repository(repo):
 def open_issues_tab():
     browser.all('.menu-item').element_by(have.text('Issues')).click()
 
-@allure.step('Проверяем наличик Issue с номером {number}')
+@allure.step('Проверяем наличие Issue с номером {number}')
 def shoild_see_issue_with_number(number):
     browser.element('.codesearch-results').should(have.text(number))
 
-    
+
+@allure.tag("web")
+@allure.severity(Severity.NORMAL)
+@allure.label("owner", "krivoruchenko_an")
+@allure.feature("Задачи в репозитории")
+@allure.story("Авторизованный пользователь может создать задачу в репозитории")
+@allure.link("https://github.com", name="Testing")
+def test_decorator_labels():
+    pass
 
